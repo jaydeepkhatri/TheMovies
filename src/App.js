4a05c2ecba4d76af4890b4126d180fb1d321cc13
Sudarshan Rai <rrraishati@gmail.com>
2022-03-13 22:23:20 +0545

    refactored axios paramaters

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import { useState, useEffect } from "react";
 
 
 
-const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7cfc85516ca2247cf6e74cb94dc31857";
-
-const search_API = "https://api.themoviedb.org/3/search/movie?api_key=7cfc85516ca2247cf6e74cb94dc31857&query=";
+const API = "https://api.themoviedb.org/3/discover/movie";
+const api_key="7cfc85516ca2247cf6e74cb94dc31857";
+const search_API = "https://api.themoviedb.org/3/search/movie";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [genre, setGenre] = useState([]);
 
   useEffect(() => {
-    axios.get(API)
+    axios.get(API,{
+      params:{
+        api_key,
+        sort_by:"popularity.desc"
+      }
+    })
       .then(res => {
         setMovies(res.data.results);
       });
   }, []);
   
   const searchMovie = (search) => {
-      axios.get(search_API + search)
+      axios.get(search_API,{
+        params:{
+          api_key,
+          query:search
+        }
+      })
         .then(res => {
           setMovies(res.data.results);
         });
